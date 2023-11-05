bc0221900dcb01b504fcd0f948a7e289947b9250
YoussefMP <ameuryoussef94@gmail.com>
2023-11-05 22:13:51 +0100

    Refractor fix

--- Source/DataCollectionNPreprocessing/Confluence_Manager/confluence_handler.py
+++ Source/DataCollectionNPreprocessing/Confluence_Manager/confluence_handler.py
@@ -6,7 +6,7 @@ V 1.0 > 21.10.2023
 __DEBUG__ = True
 
 #########################################################################################################
-from Source.DataCollectionNPreprocessing.Preprocessing.html_preprocessing import process_html_content
+from Source.DataCollectionNPreprocessing.WebScraper.html_preprocessing import process_html_content
 from Source.DataCollectionNPreprocessing.IO.io_operations import save_node_as_document
 from space_model_handler import PageNode
 from atlassian import Confluence
@@ -123,7 +123,7 @@ if __name__ == "__main__":
 
         e = "y.ameur.mail@gmail.com"
         # at = os.getenv("CONF_TOKEN")
-        with open("../../_conf/Conf_api_token.txt", "r", encoding="utf-8") as token_file:
+        with open("../../0_conf/Conf_api_token.txt", "r", encoding="utf-8") as token_file:
             at = token_file.readline()
 
         u = "https://rag-1-prototype.atlassian.net/wiki"
