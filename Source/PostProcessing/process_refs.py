0953ca373427568d77efbbac60975991c7c771d8
YoussefMP <ameuryoussef94@gmail.com>
2024-07-18 11:08:18 +0200

    process_refs.py - Cleaning uncertainties

--- Source/PostProcessing/process_refs.py
+++ Source/PostProcessing/process_refs.py
@@ -1,13 +1,30 @@
-import json 
+import faulthandler
 from Utils import paths
-import os 
+import json
+import os
+import re
+from Utils.io_operations import dump_to_jsonl
 
 on_the_fly = False
 DEBUG = False
 
 CONFIG = {"MODEL_VERSION": "v0.7",
-          "THRESHOLD": 7,
+          "THRESHOLD": 15,
           }
+
+
+KEYWORDS = {
+    "Kapitel(?:s|n)?",
+    "Absatz(?:es|e|.)?",
+    "Absätze(?:n)?",
+    "Artikel(?:s|n)?",
+    "Nr\\.?|Nummer(?:n)?",
+    "§",
+    "Satz(?:es|e|en|.)?",
+    "Sätze(?:n)?",
+    "Halbsatz"
+}
+pattern = re.compile(r'(?:\W|^)(?:' + '|'.join(KEYWORDS) + r')(?:\W|$)?', re.IGNORECASE)
 
 
 def extract_and_consolidate(entry):
@@ -46,9 +63,11 @@ def extract_and_consolidate(entry):
 
             # Set a limit of 7 chars of distance between blocks for combinations
             # If the two predictions were made with certainty we keep them separate
+            # except if the second block contains the book or law to which it is being referenced
             # got_one for debugging purposes
             # last_tag to keep track of the last fused block
-            if end + CONFIG["THRESHOLD"] >= n_start and not (last_tag == n_tag == "Ref"):
+            if (end + CONFIG["THRESHOLD"] >= n_start):
+                    # and (not (last_tag == n_tag == "Ref") or ("gesetz" in text or "buch" in text))):
                 got_one = True
                 end = n_end
                 last_tag = n_tag
@@ -79,23 +98,107 @@ def extract_and_consolidate(entry):
     return entry
 
 
-def clean_uncertain_predictions():
-    return
+def clean_uncertain_predictions(entry):
+    """
+    This method removes uncertain predictions that do not meet certain criteria.
+    :param entry: dictionary containing "text" and "label" fields
+    :return:
+    """
+    keywords = ["gesetz", "Art", "bis", "abs", "buch", "titel", "abschnitt"]
 
+    labels = entry["label"]
+    sorted_labels = sorted(labels, key=lambda x: x[0])
+
+    to_delete = []  # "Uncertain" classified spans that are faulty positives that should be deleted
+
+    for ref_id, ref in enumerate(sorted_labels):
+        s, e, label = ref
+        pre, suc = None, None   # Placeholders for the following and preceding labeled spans
+
+        if label == "Uncertain":
+            ref_span = entry["text"][s:e]
+            to_delete.append(ref_id)
+            if is_certain(ref_span) or any([kw in ref_span for kw in keywords]):
+                # Loop to find the first valid preceding classified span
+                it_finder = ref_id-1
+                while it_finder in to_delete and it_finder >= 0:
+                    it_finder -= 1
+                if it_finder >= 0 and ref[0] - sorted_labels[it_finder][1] < 25:
+                    pre = sorted_labels[it_finder]
+
+                # Loop to find the first valid following classified span
+                it_finder = ref_id+1
+                while it_finder in to_delete and it_finder < len(sorted_labels):
+                    it_finder += 1
+                if ref_id < len(sorted_labels) -1:
+                    suc = sorted_labels[ref_id+1]
+
+                # Consolidating the "uncertain" classified span with another span
+                if pre is None and suc is None:
+                    continue
+                if suc is not None and pre is not None:
+                    if ref[0] - pre[1] < suc[0] - ref[1]:
+                        pre[1] = ref[1]
+                    else:
+                        suc[0] = ref[0]
+                elif suc is None:
+                    pre[1] = ref[1]
+                elif pre is None:
+                    suc[0] = ref[0]
+
+    # parse the to_delete list in reverse order
+    for d_id in to_delete[::-1]:
+        # delete d_id for sorted_labels
+        del sorted_labels[d_id]
+
+    entry["label"] = sorted_labels
+    return entry
+
+
+def print_labeled_sequence(tokenizer, input_ids, labels):
+    """
+    Prints the input sequence along with the labels.
+    :param tokenizer: tokenizer object
+    :param input_ids: input sequence
+    :param labels: labels
+    :return:
+    """
+    tokens = tokenizer.convert_ids_to_tokens(input_ids)
+    for token, label in zip(tokens, labels):
+        print(f"{token}: {label}")
+
+
+def is_certain(span):
+    return bool(pattern.search(span))
 
 
 if __name__ == '__main__' and not on_the_fly:
 
+    faulthandler.enable()
+
+    # Cleaned list of entries
+    cleaned_entries = []
+
+    # Define the path to the output file
+    output_file = os.path.join(paths.annotations_folder, "V3_Cleaned_dataset.jsonl")
+
     # Define the path to the model output folder
     files_folder = os.path.join(paths.model_output_folder, CONFIG["MODEL_VERSION"])
-    
     # list only files in the model output folder
     files = [f for f in os.listdir(files_folder) if os.path.isfile(os.path.join(files_folder, f))]
-        
+
     for file in files:
         # Load the dataset from the JSON file
         with open(os.path.join(files_folder, file), 'r', encoding="utf-8") as f:
             for line in f.readlines():
                 entry_object = json.loads(line)
-                extract_and_consolidate(entry_object)
-        break
+
+                # processed_entry = clean_uncertain_predictions(extract_and_consolidate(entry_object))
+                # cleaned_entries.append(processed_entry)
+
+
+
+        f.close()
+
+    # Write the cleaned entries to the output file
+    dump_to_jsonl(output_file, cleaned_entries)
---
+++ Source/Utils/keywords.py
@@ -0,0 +1,35 @@
+KEYWORDS = [
+    "§",
+    "Absatz",
+    "Satz",
+    "Abs.",
+    "Artikel",
+]
+
+
+"""
+Kapitel
+
+Absatzes
+Absätze
+Abs
+
+Artikels
+Artikeln
+
+Nr.
+Nr
+Nummer
+Nummern
+
+§
+Absatz
+Satz
+Abs.
+Artikel
+
+Satzes
+Sätze
+Sätzen
+Halbsatz
+"""
